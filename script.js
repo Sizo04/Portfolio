b3d22d996f7c17e235dedd5ed47d61a5d502c081
Sizo04 <sizowork@gmail.com>
2023-07-10 18:42:50 +0200

    done

---
+++ script.js
@@ -0,0 +1,9 @@
+let buttons = document.querySelectorAll(".demo");
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        window.alert('This website is not hosted at the moment')
+    })
+})
+
+    
