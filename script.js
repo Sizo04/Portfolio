1734bf5054ff7722bec2eb4382833bc62f1836cf
Sizo04 <sizowork@gmail.com>
2024-05-30 22:35:27 +0200

    nav links

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 let buttons = document.querySelectorAll(".demo");
 
+const navLinks = document.querySelectorAll(".nav-link");
+
+console.log(navLinks);
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        const activeLink = document.querySelector(".nav-link.active");
+        if (activeLink && activeLink != link) {
+            activeLink.classList.toggle("active");
+        }
+        link.classList.toggle("active");
+
+
+
+    })
+
+
+})
+
+
+
+
+
 function Learn() {
     let about = document.getElementById("about");
     about.scrollIntoView();
